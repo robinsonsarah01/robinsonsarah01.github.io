f297f25bdd707a74d7dafe61939fa7baf8e57fc4
Sarah Robinson <robin26s@mtholyoke.edu>
2016-07-17 19:56:39 -0700

    Partial rewrite of experience page.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [robinsonsarah01.github.io](http://robinsonsarah01.github.io/)
 =========================
 
-Personal website. Redesigned in July 2016 using [MaterialCSS](http://materializecss.com/).
+Simple personal website. Redesigned in July 2016 using [MaterializeCSS](http://materializecss.com/).
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    // Add an extra-deep shadow when someone hovers over a card/project.
+    // Add an extra-deep shadow when someone hovers over a card.
     // .hoverable has a similar effect, but the shadow isn't as deep.
-    $(".card").hover(function(event){
+    $(".card").hover(function(event) {
         $(this).toggleClass("z-depth-3");
     });
 });
